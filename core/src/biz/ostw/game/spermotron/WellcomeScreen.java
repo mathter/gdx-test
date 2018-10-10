24f752728519be96530fea6418ecf577dcd47c01
mathter <mathter@mail.ru>
2018-10-10 18:56:10 +0700

    Images

--- core/src/biz/ostw/game/spermotron/WellcomeScreen.java
+++ core/src/biz/ostw/game/spermotron/WellcomeScreen.java
@@ -2,46 +2,73 @@ package biz.ostw.game.spermotron;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+import javax.xml.soap.Text;
+
+import biz.ostw.libgdx.DrawUtils;
 
 public class WellcomeScreen extends ScreenAdapter {
 
     private SpriteBatch batch;
 
+    private OrthographicCamera camera;
+
+    private BitmapFont font;
+
     private Texture texture;
 
-    private OrthographicCamera camera;
+    private float width = Gdx.graphics.getWidth();
+
+    private float height = Gdx.graphics.getHeight();
 
     @Override
     public void show() {
 
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
+        this.texture = new Texture(Gdx.files.internal("pregnant.png"));
 
         this.camera = new OrthographicCamera();
-        this.camera.setToOrtho(false, w, h);
+        this.camera.setToOrtho(false, this.width, this.height);
         this.camera.position.set(0, 0, 0);
 
         this.batch = new SpriteBatch();
-        this.texture = new Texture(Gdx.files.internal("badlogic.jpg"));
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("typesimp.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 150;
+        parameter.characters = FreeTypeFontGenerator.DEFAULT_CHARS;
+
+        this.font = generator.generateFont(parameter);
+
+        generator.dispose();
     }
 
     @Override
     public void render(float delta) {
 
-        Gdx.gl.glClearColor(0.2f,0.2f,0f,1f);
-        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-        this.camera.rotate(0.2f);
         this.camera.update();
 
         this.batch.setProjectionMatrix(this.camera.combined);
         this.batch.begin();
-        this.batch.draw(this.texture, 0,0);
+
+        DrawUtils.drawInscribed(this.batch, this.texture, -this.width / 5, -this.height / 2, this.width, this.height);
+
+        GlyphLayout gl = new GlyphLayout();
+
+        gl.setText(this.font, "S P E R M O T R O N !");
+
+        this.font.draw(this.batch, gl, -gl.width / 2, gl.height / 2);
+
         this.batch.end();
     }
 
@@ -49,6 +76,8 @@ public class WellcomeScreen extends ScreenAdapter {
     public void dispose() {
 
         this.batch.dispose();
-        this.texture.dispose();
+        this.font.dispose();
     }
+
+
 }
---
+++ core/src/biz/ostw/libgdx/DrawUtils.java
@@ -0,0 +1,31 @@
+package biz.ostw.libgdx;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+public class DrawUtils {
+
+    public static void drawInscribed(Batch batch, Texture texture, float x, float y, float width, float height) {
+
+        final float textureWidth = texture.getWidth();
+        final float textureHeight = texture.getHeight();
+
+        final float widthRatio = textureWidth / width;
+        final float heightRatio = textureHeight / height;
+        final float ratio;
+
+        if (widthRatio < heightRatio) {
+            ratio = heightRatio;
+        } else {
+            ratio = widthRatio;
+        }
+
+        width = textureWidth / ratio;
+        height = textureHeight / ratio;
+
+        batch.draw(texture, x, y, width, height);
+    }
+
+    private DrawUtils() {
+    }
+}
