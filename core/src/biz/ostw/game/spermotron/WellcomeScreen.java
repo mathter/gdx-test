1c224558ce36cf2dd8d2b6953903e39ebf2c9046
mathter <mathter@mail.ru>
2018-10-16 00:29:45 +0700

    Images

--- core/src/biz/ostw/game/spermotron/WellcomeScreen.java
+++ core/src/biz/ostw/game/spermotron/WellcomeScreen.java
@@ -11,28 +11,32 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import biz.ostw.libgdx.DrawUtils;
 import biz.ostw.libgdx.util.FontBuilder;
 
 public class WellcomeScreen extends ScreenAdapter {
 
-    private SpriteBatch batch;
-
     private OrthographicCamera camera;
 
     private BitmapFont font;
 
-    private Texture texture;
+    private TextureAtlas atlas;
+
+    private Skin skin;
 
     private Stage stage;
+
+    private Viewport viewport;
+
+    private Texture texture;
 
     private float width = Gdx.graphics.getWidth();
 
@@ -41,19 +45,30 @@ public class WellcomeScreen extends ScreenAdapter {
     @Override
     public void show() {
 
-        this.texture = new Texture(Gdx.files.internal("pregnant.png"));
+        this.atlas = new TextureAtlas(Gdx.files.internal("pack.atlas"));
+        this.skin = new Skin(this.atlas);
 
         this.camera = new OrthographicCamera();
-        this.camera.setToOrtho(false, this.width, this.height);
-        this.camera.position.set(0, 0, 0);
+        this.viewport = new ScreenViewport();//(this.width, 1024, this.camera);
+        this.stage = new Stage(viewport);
 
-        this.font = new BitmapFont(Gdx.files.internal("data/typesimp.fnt"), Gdx.files.internal("data/typesimp.png"), false);
+        this.font = new FontBuilder(Gdx.files.internal("typesimp.ttf")).color(Color.GRAY).size(50).build();
         this.font.getData().setScale(5);
 
-        this.batch = new SpriteBatch();
+        this.stage = new Stage(this.viewport);
+        this.stage.addActor(new Actor() {
+            @Override
+            public void draw(Batch batch, float parentAlpha) {
+                batch.draw(skin.getRegion("wellcome/pregnant"), this.getStage().getWidth() * 2 / 5, 0);
 
-        this.stage = new Stage(new ScreenViewport(), this.batch);
-        this.stage.addActor(this.buildTextButton("GO!"));
+                GlyphLayout gl = new GlyphLayout();
+
+                gl.setText(WellcomeScreen.this.font, "S P E R M O T R O N !");
+
+                WellcomeScreen.this.font.draw(batch, gl, (stage.getWidth() - gl.width) / 2, (stage.getHeight() - gl.height) / 2);
+            }
+        });
+        this.stage.addActor(this.buildTextButton("GO!", 0, 0));
 
         Gdx.input.setInputProcessor(this.stage);
     }
@@ -64,50 +79,32 @@ public class WellcomeScreen extends ScreenAdapter {
         Gdx.gl.glClearColor(0.2f, 0.2f, 0f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-        this.camera.update();
 
-        this.batch.setProjectionMatrix(this.camera.combined);
-        this.batch.begin();
-
-        DrawUtils.drawInscribed(this.batch, this.texture, -this.width / 5, -this.height / 2, this.width, this.height);
-
-        GlyphLayout gl = new GlyphLayout();
-
-        gl.setText(this.font, "S P E R M O T R O N !");
-
-        this.font.draw(this.batch, gl, -gl.width / 2, gl.height / 2);
-
-        this.batch.end();
-
-        this.stage.act();
         this.stage.draw();
     }
 
-    private TextButton buildTextButton(String text) {
-        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("ui/ui.atlas"));
+    private TextButton buildTextButton(String text, final int x, final int y) {
         Skin skin = new Skin(atlas);
 
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
-        style.up = skin.getDrawable("button");
-        style.over = skin.getDrawable("button");
-        style.down = skin.getDrawable("button_pressed");
+        style.up = skin.getDrawable("ui/button");
+        style.over = skin.getDrawable("ui/button");
+        style.down = skin.getDrawable("ui/button_pressed");
         style.pressedOffsetX = 1;
         style.pressedOffsetY = -1;
         style.font = this.font;
 
-        return new TextButton(text, style){
+        return new TextButton(text, style) {
             {
-                this.setPosition(400,400);
+                this.setPosition(x, y);
             }
         };
     }
 
     @Override
     public void dispose() {
-
-        this.batch.dispose();
+        this.atlas.dispose();
+        this.skin.dispose();
         this.font.dispose();
     }
-
-
 }
